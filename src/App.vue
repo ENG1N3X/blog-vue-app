f41db1cb3074c821d315dea5d6a25aef6e679f2e
eng1n3x <eng1n3x.developer@gmail.com>
2022-08-17 13:35:30 +0300

    feat: page container

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="app">
+	<div id="app" class="max-w-screen-2xl mx-auto px-4">
 		<div id="nav" class="text-center p-7">
 			<router-link to="/" class="font-bold">Home</router-link>
 		</div>
